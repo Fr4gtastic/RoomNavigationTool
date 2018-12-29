9507a363579d8afc11fa8989befbf543cb61306a
Fr4gtastic <milosz.swierad@gmail.com>
2018-12-29 13:16:08 +0100

    Create confusion matrix and add comments

--- network_training.py
+++ network_training.py
@@ -47,21 +47,29 @@ validation_generator = test_data_generator.flow_from_directory(
 
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.8,
                               patience=10, min_lr=1e-8)
+# reduce learning rate by [factor] if [monitor] hasn't changed after [patience] epochs until it reaches the value of
+# [min_lr]
 
 tensor_board = TensorBoard(log_dir=tensor_board_dir,
-                           batch_size=32,
+                           batch_size=batch_size,
                            write_graph=True,
                            write_grads=True,
                            write_images=True,
                            update_freq='epoch')
+# visualization callback; to see the results use the command
+# python -m tensorboard.main --logdir ./logs
+# and go to
+# http://localhost:6006
 
 model_checkpoint = ModelCheckpoint(filepath=model_filename)
+# save the model after each epoch
 
 early_stopping = EarlyStopping(monitor='val_loss',
                                min_delta=0.001,
                                patience=10,
                                verbose=1,
                                mode='auto')
+# stop training if [monitor] hasn't changed after [patience] epochs by at least [min_delta]
 
 learning_rate_scheduler = LearningRateScheduler(step_decay)
 
@@ -71,12 +79,23 @@ history = model.fit_generator(
     epochs=epochs,
     validation_data=validation_generator,
     validation_steps=nb_validation_samples // batch_size,
-    callbacks=[tensor_board, model_checkpoint, learning_rate_scheduler, early_stopping])
+    callbacks=[tensor_board, model_checkpoint, reduce_lr])
 
 model.save(model_filename)
 
 plot_training.plot_accuracy(history)
 plot_training.plot_loss(history)
 
-# python -m tensorboard.main --logdir ./logs
-# http://localhost:6006
+
+validation_predict = model.predict_generator(validation_generator, nb_validation_samples // batch_size + 1)
+# make predictions for validation set
+validation_predict_y_max = np.argmax(validation_predict, axis=1)
+print('Confusion matrix')
+confusion_matrix = confusion_matrix(validation_generator.classes, validation_predict_y_max)
+print(confusion_matrix)
+print('\n')
+print('Classification report')
+target_names = validation_generator.class_indices
+print(classification_report(validation_generator.classes, validation_predict_y_max, target_names=target_names))
+plot_training.plot_conf_matrix(confusion_matrix, nb_validation_samples)
+# plot the confusion matrix
