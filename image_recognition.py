9ee801a07371b18daa4de3c86bbd6b141a2aef62
Fr4gtastic <milosz.swierad@gmail.com>
2019-01-16 03:25:23 +0100

    Trying modified architecture

--- image_recognition.py
+++ image_recognition.py
@@ -1,33 +1,43 @@
 from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, BatchNormalization
 from keras.optimizers import Adam
 
-img_width = 32
-img_height = 32
+img_width = 64
+img_height = 64
 filename = 'model.h5'
 
 model = Sequential()
 model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(img_width, img_height, 3)))
+model.add(BatchNormalization())
 model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
 model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
 model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
 
 model.add(Conv2D(128, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
 model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(BatchNormalization())
 model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
+
+model.add(Conv2D(256, (3, 3), padding='same', activation='relu'))
+model.add(BatchNormalization())
+model.add(Conv2D(256, (3, 3), activation='relu'))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
 
 model.add(Flatten())
-model.add(Dense(256, activation='relu'))
+model.add(Dense(512, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(3, activation='softmax'))
 
 optimizer = Adam(
+    lr=0.001,
     beta_1=0.9,
     beta_2=0.999,
     epsilon=1e-8)
--- network_training.py
+++ network_training.py
@@ -11,15 +11,16 @@ from step_decay import step_decay
 from sklearn.metrics import confusion_matrix, classification_report
 from samples_counter import count_samples
 
-train_data_dir = r'data/train'
+train_data_dir = r'data4/train'
 validation_data_dir = r'data/validation'
 tensor_board_dir = r'./logs'
+validation_split_factor = 0.2
 nb_train_samples = count_samples(train_data_dir)
-nb_validation_samples = count_samples(validation_data_dir)
-epochs = 100
-batch_size = 16
-img_width = 32
-img_height = 32
+nb_validation_samples = int(nb_train_samples * validation_split_factor)
+epochs = 200
+batch_size = 32
+img_width = 64
+img_height = 64
 model_filename = 'model.h5'
 
 model = load_model(model_filename)
@@ -29,7 +30,8 @@ train_data_generator = ImageDataGenerator(
     width_shift_range=0.2,
     height_shift_range=0.2,
     zoom_range=0.2,
-    horizontal_flip=True)
+    horizontal_flip=True,
+    validation_split=validation_split_factor)
 
 test_data_generator = ImageDataGenerator(rescale=1. / 255)
 
@@ -37,16 +39,18 @@ train_generator = train_data_generator.flow_from_directory(
     train_data_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
-    class_mode='categorical')
+    class_mode='categorical',
+    subset='training')
 
-validation_generator = test_data_generator.flow_from_directory(
-    validation_data_dir,
+validation_generator = train_data_generator.flow_from_directory(
+    train_data_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
-    class_mode='categorical')
+    class_mode='categorical',
+    subset='validation')
 
-reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.8,
-                              patience=10, min_lr=1e-8)
+reduce_lr = ReduceLROnPlateau(monitor='val_acc', factor=0.1,
+                              patience=5, min_lr=0, min_delta=1e-3)
 # reduce learning rate by [factor] if [monitor] hasn't changed after [patience] epochs until it reaches the value of
 # [min_lr]
 
@@ -65,21 +69,22 @@ model_checkpoint = ModelCheckpoint(filepath=model_filename)
 # save the model after each epoch
 
 early_stopping = EarlyStopping(monitor='val_loss',
-                               min_delta=0.001,
+                               min_delta=1e-3,
                                patience=10,
                                verbose=1,
-                               mode='auto')
+                               mode='auto',
+                               restore_best_weights=True)
 # stop training if [monitor] hasn't changed after [patience] epochs by at least [min_delta]
 
 learning_rate_scheduler = LearningRateScheduler(step_decay)
 
 history = model.fit_generator(
     train_generator,
-    steps_per_epoch=nb_train_samples // batch_size,
+    steps_per_epoch=5*nb_train_samples // batch_size,
     epochs=epochs,
     validation_data=validation_generator,
-    validation_steps=nb_validation_samples // batch_size,
-    callbacks=[tensor_board, model_checkpoint, reduce_lr])
+    validation_steps=5*nb_validation_samples // batch_size,
+    callbacks=[tensor_board, model_checkpoint, learning_rate_scheduler])
 
 model.save(model_filename)
 
