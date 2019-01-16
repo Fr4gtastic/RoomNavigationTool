6c78304b1954d33e34a3088d74ff7e1a7d06aee7
Fr4gtastic <milosz.swierad@gmail.com>
2019-01-16 03:24:24 +0100

    Change drop and learning rate values

--- step_decay.py
+++ step_decay.py
@@ -2,8 +2,8 @@ import math
 
 
 def step_decay(epoch):
-    initial_learning_rate = 0.1
-    drop = 0.5
+    initial_learning_rate = 0.01
+    drop = 0.75
     epochs_drop = 5.0
     lrate = initial_learning_rate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
     return lrate
