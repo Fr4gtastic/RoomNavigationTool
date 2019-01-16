e19ccd83f50863619f0f78ed8d83a4b8afd69f70
Fr4gtastic <milosz.swierad@gmail.com>
2019-01-16 02:06:17 +0100

    Change number of epochs between steps

--- step_decay.py
+++ step_decay.py
@@ -4,6 +4,6 @@ import math
 def step_decay(epoch):
     initial_learning_rate = 0.1
     drop = 0.5
-    epochs_drop = 10.0
+    epochs_drop = 5.0
     lrate = initial_learning_rate * math.pow(drop, math.floor((1+epoch)/epochs_drop))
     return lrate
